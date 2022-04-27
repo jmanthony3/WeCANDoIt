a0e7f263a275ebeb276b5824a76b344e4a05ad40
Joby Anthony III <62029937+jmanthony3@users.noreply.github.com>
2022-04-27 13:10:20 -0400

    skeleton

---
+++ Asciidoc/Document/make/build/asciidoctor_runner.rb
@@ -0,0 +1,7 @@
+require 'asciidoctor'
+require 'asciidoctor-bibtex'
+Asciidoctor.convert_file 'C:\Users\jmanthony1\Documents\GitHub\WeCANDoIt\Asciidoc\Document\make\build\intermediate\ENGR-527_727-WeCANDoIt-Final_Project.adoc', to_file: 'C:\Users\jmanthony1\Documents\GitHub\WeCANDoIt\Asciidoc\Document\ENGR-527_727-WeCANDoIt-Final_Project.html', safe: :unsafe, backend: 'html5', mkdirs: true, basedir: 'C:\Users\jmanthony1\Documents\GitHub\WeCANDoIt\Asciidoc\Document\make\build\intermediate', attributes: 'imagesdir@=''images'' stylesheet@=''asciidoxy-no-toc.css'' multipage'
+logger = Asciidoctor::LoggerManager.logger
+exit 1 if (logger.respond_to? :max_severity) &&
+  logger.max_severity &&
+  logger.max_severity >= (::Logger::Severity.const_get 'FATAL')
