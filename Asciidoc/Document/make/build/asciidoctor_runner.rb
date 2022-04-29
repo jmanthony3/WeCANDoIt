e0690ea61b153ee34dc68f7ea9a38246e98e740d
Joby Anthony III <62029937+jmanthony3@users.noreply.github.com>
2022-04-29 11:21:52 -0400

    initial pass of edits.

--- Asciidoc/Document/make/build/asciidoctor_runner.rb
+++ Asciidoc/Document/make/build/asciidoctor_runner.rb
@@ -1,6 +1,6 @@
 require 'asciidoctor'
 require 'asciidoctor-bibtex'
-Asciidoctor.convert_file 'C:\Users\cfarmer6\Documents\GitHub\WeCANDoIt\Asciidoc\Document\make\build\intermediate\ENGR-527_727-WeCANDoIt-Final_Project.adoc', to_file: 'C:\Users\cfarmer6\Documents\GitHub\WeCANDoIt\Asciidoc\Document\ENGR-527_727-WeCANDoIt-Final_Project.html', safe: :unsafe, backend: 'html5', mkdirs: true, basedir: 'C:\Users\cfarmer6\Documents\GitHub\WeCANDoIt\Asciidoc\Document\make\build\intermediate', attributes: 'imagesdir@=''images'' stylesheet@=''asciidoxy-no-toc.css'' multipage'
+Asciidoctor.convert_file 'C:\Users\jmanthony1\Documents\GitHub\WeCANDoIt\Asciidoc\Document\make\build\intermediate\ENGR-527_727-WeCANDoIt-Final_Project.adoc', to_file: 'C:\Users\jmanthony1\Documents\GitHub\WeCANDoIt\Asciidoc\Document\ENGR-527_727-WeCANDoIt-Final_Project.html', safe: :unsafe, backend: 'html5', mkdirs: true, basedir: 'C:\Users\jmanthony1\Documents\GitHub\WeCANDoIt\Asciidoc\Document\make\build\intermediate', attributes: 'imagesdir@=''images'' stylesheet@=''asciidoxy-no-toc.css'' multipage'
 logger = Asciidoctor::LoggerManager.logger
 exit 1 if (logger.respond_to? :max_severity) &&
   logger.max_severity &&
